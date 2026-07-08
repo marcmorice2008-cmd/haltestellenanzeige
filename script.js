a6eb0e5a418fcaf3daaef9a77a5783f70a18fe6d
marcmorice2008-cmd <marcmorice2008@gmail.com>
2026-07-08 17:35:57 +0200

    script.js aktualisieren

--- script.js
+++ script.js
@@ -1,9 +1,77 @@
-function uhrAktualisieren() {
-    const jetzt = new Date();
+const HALTESTELLE = "de:07339:32685";
 
+function uhrAktualisieren() {
     document.getElementById("uhr").textContent =
-        jetzt.toLocaleTimeString("de-DE");
+        new Date().toLocaleTimeString("de-DE");
 }
 
 setInterval(uhrAktualisieren, 1000);
 uhrAktualisieren();
+
+
+async function abfahrtenLaden() {
+
+    try {
+        /*
+          Platzhalter für Live-API.
+          Hier kommt der RNN/VRN-Datenabruf hinein.
+        */
+
+        const daten = [
+            {
+                linie: "654",
+                ziel: "Mainz Hbf",
+                zeit: "2 min"
+            },
+            {
+                linie: "630",
+                ziel: "Bad Kreuznach Bf",
+                zeit: "11 min"
+            },
+            {
+                linie: "653",
+                ziel: "Mainz Hbf",
+                zeit: "19 min"
+            },
+            {
+                linie: "656",
+                ziel: "Nieder-Olm",
+                zeit: "27 min"
+            }
+        ];
+
+        const tabelle = document.querySelector(".tabelle");
+
+        tabelle.innerHTML = `
+        <div class="zeile kopfzeile">
+            <span>LINIE</span>
+            <span>ZIEL</span>
+            <span>AB</span>
+        </div>
+        `;
+
+        daten.forEach(bus => {
+
+            tabelle.innerHTML += `
+            <div class="zeile">
+                <span>${bus.linie}</span>
+                <span>${bus.ziel}</span>
+                <span>${bus.zeit}</span>
+            </div>
+            `;
+
+        });
+
+    }
+    catch(e) {
+        console.log("Fehler beim Laden", e);
+    }
+}
+
+
+abfahrtenLaden();
+
+setInterval(
+    abfahrtenLaden,
+    30000
+);
