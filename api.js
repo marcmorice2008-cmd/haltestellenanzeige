65854658f6abe76ba32a25368b889f45deb0681c
marcmorice2008-cmd <marcmorice2008@gmail.com>
2026-07-08 23:27:19 +0200

    api.js aktualisieren

--- api.js
+++ api.js
@@ -2,23 +2,49 @@ async function ladeAbfahrten() {
 
     const HALTESTELLE = "de:07339:32685";
 
-    console.log(
-        "Lade Abfahrten für:",
-        HALTESTELLE
-    );
+    try {
+
+        /*
+        Hier kommt später die echte TRIAS-Anfrage hin.
+        Die Antwort wird danach in unser Anzeigeformat umgewandelt.
+        */
 
 
-    // Test-Rückgabe
-    return [
-        {
-            linie: "654",
-            ziel: "Mainz Hbf",
-            zeit: "2 min"
-        },
-        {
-            linie: "630",
-            ziel: "Bad Kreuznach Bf",
-            zeit: "11 min"
-        }
-    ];
+        const daten = [
+            {
+                linie: "654",
+                ziel: "Mainz Hbf",
+                zeit: "2 min"
+            },
+            {
+                linie: "630",
+                ziel: "Bad Kreuznach Bf",
+                zeit: "11 min"
+            },
+            {
+                linie: "653",
+                ziel: "Mainz Hbf",
+                zeit: "19 min"
+            },
+            {
+                linie: "656",
+                ziel: "Nieder-Olm",
+                zeit: "27 min"
+            }
+        ];
+
+
+        return daten;
+
+
+    } catch(error) {
+
+        console.error(
+            "Fehler beim Laden:",
+            error
+        );
+
+        return [];
+
+    }
 }
